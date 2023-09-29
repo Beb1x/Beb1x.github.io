9c913aef21630b8c19e42961ef49be192a4acb47
Beb1x <146170241+Beb1x@users.noreply.github.com>
2023-09-30 02:38:47 +0300

    Update backround.js

--- backround.js
+++ backround.js
@@ -1,28 +1,54 @@
 const canvas = document.createElement('canvas');
 const ctx = canvas.getContext('2d');
 document.body.appendChild(canvas);
-function initializeCanvas() {
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
-    ctx.fillStyle = '#00FFFF';
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
-}
-function drawCloud(ctx, x, y, size) {
+class Cloud {
+  constructor(x, y, size) {
+    this.x = x;
+    this.y = y;
+    this.size = size;
+    this.velocity = 0.5;
+  }
+  draw(ctx) {
     ctx.fillStyle = '#FFFFFF';
     ctx.beginPath();
-    ctx.arc(x + size, y + size, size, 0, 2 * Math.PI);
-    ctx.arc(x + size * 1.5, y + size * 0.5, size * 0.75, 0, 2 * Math.PI);
-    ctx.arc(x + size * 2.5, y + size * 1.5, size, 0, 2 * Math.PI);
+    ctx.arc(this.x + this.size, this.y + this.size, this.size, 0, 2 * Math.PI);
+    ctx.arc(this.x + this.size * 1.5, this.y + this.size * 0.5, this.size * 0.75, 0, 2 * Math.PI);
+    ctx.arc(this.x + this.size * 2.5, this.y + this.size * 1.5, this.size, 0, 2 * Math.PI);
     ctx.fill();
-}
-function drawRandomClouds() {
-    for (let i = 0; i < 10; i++) {
-        drawCloud(ctx, Math.random() * canvas.width, Math.random() * canvas.height, Math.random() * 50 + 20);
+  }
+  update() {
+    this.x += this.velocity;
+    if (this.x > canvas.width) {
+      this.x = 0;
     }
+  }
+}
+const clouds = [];
+function initializeCanvas() {
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+  ctx.fillStyle = '#00FFFF';
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+}
+function draw() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  for (const cloud of clouds) {
+    cloud.update();
+    cloud.draw(ctx);
+  }
+  requestAnimationFrame(draw);
+}
+function generateRandomCloud() {
+  const x = Math.random() * canvas.width;
+  const y = Math.random() * canvas.height;
+  const size = Math.random() * 50 + 20;
+  return new Cloud(x, y, size);
+}
+for (let i = 0; i < 10; i++) {
+  clouds.push(generateRandomCloud());
 }
 initializeCanvas();
-drawRandomClouds();
+draw();
 window.addEventListener('resize', () => {
-    initializeCanvas();
-    drawRandomClouds();
+  initializeCanvas();
 });
