bd699243cae64a67e6dd78e20670766d4e3ab80f
Beb1x <146170241+Beb1x@users.noreply.github.com>
2023-09-30 02:23:52 +0300

    Update backround.js

--- backround.js
+++ backround.js
@@ -1,24 +1,25 @@
 const canvas = document.createElement('canvas');
 const ctx = canvas.getContext('2d');
 document.body.appendChild(canvas);
+let stickmen = [];
 function initializeCanvas() {
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
-    ctx.fillStyle = '#00FFFF';
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+  ctx.fillStyle = '#00FFFF';
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
 }
 function drawCloud(ctx, x, y, size) {
-    ctx.fillStyle = '#FFFFFF';
-    ctx.beginPath();
-    ctx.arc(x + size, y + size, size, 0, 2 * Math.PI);
-    ctx.arc(x + size * 1.5, y + size * 0.5, size * 0.75, 0, 2 * Math.PI);
-    ctx.arc(x + size * 2.5, y + size * 1.5, size, 0, 2 * Math.PI);
-    ctx.fill();
+  ctx.fillStyle = '#FFFFFF';
+  ctx.beginPath();
+  ctx.arc(x + size, y + size, size, 0, 2 * Math.PI);
+  ctx.arc(x + size * 1.5, y + size * 0.5, size * 0.75, 0, 2 * Math.PI);
+  ctx.arc(x + size * 2.5, y + size * 1.5, size, 0, 2 * Math.PI);
+  ctx.fill();
 }
 function drawRandomClouds() {
-    for (let i = 0; i < 10; i++) {
-        drawCloud(ctx, Math.random() * canvas.width, Math.random() * canvas.height, Math.random() * 50 + 20);
-    }
+  for (let i = 0; i < 10; i++) {
+    drawCloud(ctx, Math.random() * canvas.width, Math.random() * canvas.height, Math.random() * 50 + 20);
+  }
 }
 function drawStickman(ctx, x, y) {
   ctx.fillStyle = '#000';
@@ -43,25 +44,29 @@ function drawStickman(ctx, x, y) {
   ctx.lineTo(x + 30, y + 50);
   ctx.stroke();
 }
-}
 function generateRandomStickmanPosition() {
-    const x = Math.random() * canvas.width;
-    const y = Math.random() * canvas.height;
-    return { x, y };
+  const x = Math.random() * canvas.width;
+  const y = Math.random() * canvas.height;
+  return { x, y };
+}
+function draw() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  drawRandomClouds();
+  for (const stickman of stickmen) {
+    drawStickman(ctx, stickman.x, stickman.y);
+  }
+  requestAnimationFrame(draw);
 }
 initializeCanvas();
 drawRandomClouds();
-const numberOfStickmen = 15;
-const stickmen = [];
-for (let i = 0; i < numberOfStickmen; i++) {
-    const position = generateRandomStickmanPosition();
-    stickmen.push(position);
-    drawStickman(ctx, position.x, position.y);
+for (let i = 0; i < 15; i++) {
+  stickmen.push(generateRandomStickmanPosition());
 }
+requestAnimationFrame(draw);
 window.addEventListener('resize', () => {
-    initializeCanvas();
-    drawRandomClouds();
-    stickmen.forEach((position) => {
-        drawStickman(ctx, position.x, position.y);
-    });
+  initializeCanvas();
+  drawRandomClouds();
+  stickmen.forEach((stickman) => {
+    drawStickman(ctx, stickman.x, stickman.y);
+  });
 });
