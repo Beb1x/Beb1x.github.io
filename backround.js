4d5c13834f269aa251778f8c42b668110b822ad0
Beb1x <146170241+Beb1x@users.noreply.github.com>
2023-09-30 23:23:27 +0300

    Update backround.js

--- backround.js
+++ backround.js
@@ -2,31 +2,6 @@ const canvas = document.createElement('canvas');
 const ctx = canvas.getContext('2d');
 document.body.appendChild(canvas);
 
-class Cloud {
-  constructor(x, y, size, velocity) {
-    this.x = x;
-    this.y = y;
-    this.size = size;
-    this.velocity = velocity;
-  }
-  draw(ctx) {
-    ctx.fillStyle = '#FFFFFF';
-    ctx.beginPath();
-    ctx.arc(this.x + this.size, this.y + this.size, this.size, 0, 2 * Math.PI);
-    ctx.arc(this.x + this.size * 1.5, this.y + this.size * 0.5, this.size * 0.75, 0, 2 * Math.PI);
-    ctx.arc(this.x + this.size * 2.5, this.y + this.size * 1.5, this.size, 0, 2 * Math.PI);
-    ctx.fill();
-  }
-  update() {
-    this.x += this.velocity;
-    if (this.y > canvas.height) {
-      this.y = -this.size;
-      this.x = Math.random() * (canvas.width - 100) + 50;
-    }
-  }
-}
-
-const clouds = [];
 const stickmen = [];
 
 function initializeCanvas() {
@@ -42,10 +17,6 @@ function initializeCanvas() {
 
 function draw() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
-  for (const cloud of clouds) {
-    cloud.update();
-    cloud.draw(ctx);
-  }
   for (const stickman of stickmen) {
     changeStickmanDirection(stickman);
     if (stickman.offsetLeft < 0.8 || stickman.offsetLeft > canvas.width) {
@@ -57,26 +28,6 @@ function draw() {
   }
   requestAnimationFrame(draw);
 }
-
-function generateRandomCloud() {
-  const x = Math.random() * (canvas.width - 100) + 50;
-  const y = Math.random() * (canvas.height / 2);
-  const size = Math.random() * 45 + 20;
-  const velocity = Math.random() * 0.1 + 0.05; 
-  return new Cloud(x, y, size, velocity);
-}
-
-for (let i = 0; i < 50; i++) { 
-  const cloud = generateRandomCloud();
-  clouds.push(cloud);
-}
-
-initializeCanvas();
-draw();
-
-window.addEventListener('resize', () => {
-  initializeCanvas();
-});
 
 function getRandomNumber(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
@@ -117,3 +68,10 @@ function resetAnimation() {
     createStickman();
   }
 }
+
+initializeCanvas();
+draw();
+
+window.addEventListener('resize', () => {
+  initializeCanvas();
+});
