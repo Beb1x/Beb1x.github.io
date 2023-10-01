056104db73a997edbe638ba0e7c7c98a7437f3a7
Beb1x <146170241+Beb1x@users.noreply.github.com>
2023-10-01 14:44:53 +0300

    Update backround.js

--- backround.js
+++ backround.js
@@ -2,7 +2,32 @@ const canvas = document.createElement('canvas');
 const ctx = canvas.getContext('2d');
 document.body.appendChild(canvas);
 
-const characters = [];
+class Cloud {
+  constructor(x, y, size, velocity) {
+    this.x = x;
+    this.y = y;
+    this.size = size;
+    this.velocity = velocity;
+  }
+  draw(ctx) {
+    ctx.fillStyle = '#FFFFFF';
+    ctx.beginPath();
+    ctx.arc(this.x + this.size, this.y + this.size, this.size, 0, 2 * Math.PI);
+    ctx.arc(this.x + this.size * 1.5, this.y + this.size * 0.5, this.size * 0.75, 0, 2 * Math.PI);
+    ctx.arc(this.x + this.size * 2.5, this.y + this.size * 1.5, this.size, 0, 2 * Math.PI);
+    ctx.fill();
+  }
+  update() {
+    this.x += this.velocity;
+    if (this.y > canvas.height) {
+      this.y = -this.size;
+      this.x = Math.random() * (canvas.width - 100) + 50;
+    }
+  }
+}
+
+const clouds = [];
+const stickmen = [];
 
 function initializeCanvas() {
   canvas.width = window.innerWidth;
@@ -10,52 +35,40 @@ function initializeCanvas() {
   ctx.fillStyle = '#00FFFF';
   ctx.fillRect(0, 0, canvas.width, canvas.height);
 
-  const numberOfCharacters = getRandomNumber(5, 25);
-
-  for (let i = 0; i < numberOfCharacters; i++) {
-    createCharacter();
+  for (let i = 0; i < 15; i++) {
+    createStickman();
   }
 }
 
 function draw() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
-  for (const character of characters) {
-    changeCharacterDirection(character);
-    if (character.offsetTop > canvas.height) {
-      removeCharacter(character);
-      createCharacter(); 
+  for (const cloud of clouds) {
+    cloud.update();
+    cloud.draw(ctx);
+  }
+  for (const stickman of stickmen) {
+    changeStickmanDirection(stickman);
+    if (stickman.offsetLeft < 0.8 || stickman.offsetLeft > canvas.width) {
+      removeStickman(stickman);
     }
+  }
+  if (stickmen.length === 0) {
+    resetAnimation();
   }
   requestAnimationFrame(draw);
 }
 
-function getRandomNumber(min, max) {
-  return Math.floor(Math.random() * (max - min + 1)) + min;
+function generateRandomCloud() {
+  const x = Math.random() * (canvas.width - 100) + 50;
+  const y = Math.random() * (canvas.height / 2);
+  const size = Math.random() * 45 + 20;
+  const velocity = Math.random() * 0.1 + 0.05; 
+  return new Cloud(x, y, size, velocity);
 }
 
-function createCharacter() {
-  const character = document.createElement('div');
-  const isSpooderman = Math.random() < 0.5;
-  character.classList.add(isSpooderman ? 'spooderman' : 'stickman');
-  character.style.left = getRandomNumber(0, window.innerWidth - 66) + 'px'; 
-  character.style.top = '0';
-  document.getElementById('characters-container').appendChild(character);
-  character.style.animationDelay = `${Math.random() * 5}s`;
-  characters.push(character);
-}
-
-function changeCharacterDirection(character) {
-  const randomDirection = Math.random();
-  if (randomDirection < 0.5) {
-    character.style.left = `${character.offsetLeft + 10}px`;
-  } else {
-    character.style.left = `${character.offsetLeft - 10}px`;
-  }
-}
-
-function removeCharacter(character) {
-  character.parentNode.removeChild(character);
-  characters.splice(characters.indexOf(character), 1);
+for (let i = 0; i < 50; i++) { 
+  const cloud = generateRandomCloud();
+  clouds.push(cloud);
 }
 
 initializeCanvas();
@@ -64,3 +77,43 @@ draw();
 window.addEventListener('resize', () => {
   initializeCanvas();
 });
+
+function getRandomNumber(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function createStickman() {
+  const stickman = document.createElement('div');
+  stickman.classList.add('stickman');
+  stickman.style.left = getRandomNumber(0, window.innerWidth) + 'px';
+  document.getElementById('stickmen-container').appendChild(stickman);
+  stickman.style.animationDelay = `${Math.random() * 5}s`;
+  stickmen.push(stickman);
+}
+
+const numberOfStickmen = getRandomNumber(2, 50);
+for (let i = 0; i < numberOfStickmen; i++) {
+  createStickman();
+}
+
+function changeStickmanDirection(stickman) {
+  const randomDirection = Math.random();
+  if (randomDirection < 3) {
+    stickman.style.left = `${stickman.offsetLeft + 10}px`;
+  } else {
+    stickman.style.left = `${stickman.offsetLeft - 10}px`;
+  }
+}
+
+function removeStickman(stickman) {
+  stickman.parentNode.removeChild(stickman);
+}
+
+function resetAnimation() {
+  while (stickmen.length > 0) {
+    removeStickman(stickmen[0]);
+  }
+  for (let i = 0; i < numberOfStickmen; i++) {
+    createStickman();
+  }
+}
